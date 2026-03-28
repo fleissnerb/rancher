568e4b6b6659abd6f5c818ee39f1fff969d81f4e
fleissnerb <36540779+fleissnerb@users.noreply.github.com>
2026-03-28 19:21:39 -0400

    Update install.sh

--- rke2/install.sh
+++ rke2/install.sh
@@ -1,3 +1,5 @@
+mkdir -p /etc/rancher/rke2/ ;
+curl -o /etc/rancher/rke2/config.yaml 
 curl -sfL https://get.rke2.io | sh - ;
 systemctl enable rke2-server.service ;
 systemctl start rke2-server.service
